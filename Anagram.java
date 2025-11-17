431b38b56989c88ae9e5ba6b729e8da6665d1c56
Ori Fine <ori.fine@post.runi.ac.il>
2025-11-17 21:45:21 +0200

    brosky testing

--- Anagram.java
+++ Anagram.java
@@ -28,16 +28,53 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
-	}
+		String word1 = preProcess(str1);
+		String word2 = preProcess(str2);
+		int sum1 = 0;
+		int sum2 = 0;
+    for (int i = 0; i < word1.length(); i++)
+    {
+      char charInWord = word1.charAt(i);
+      int locationInAscii = (int) charInWord;
+      sum1 = sum1 + locationInAscii;
+      }
+
+ for (int i = 0; i < word2.length(); i++)
+    {
+      char charInWord = word2.charAt(i);
+      int locationInAscii = (int) charInWord;
+      sum2 = sum2 + locationInAscii;
+    }
+      if (sum1 == sum2)
+       {
+		return true;
+       }
+    else
+      return false;
+	  
+
+}
+
+	
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		String word = str.toLowerCase();
+        String newWord = ""; 
+    for (int i = 0; i < word.length(); i++)
+    {
+      char charInWord = word.charAt(i);
+      int locationInAscii = (int) charInWord;
+      if (locationInAscii > 96)
+      {
+        newWord = newWord + charInWord;
+      }
+
+    }
+    System.out.println(newWord);
+	return newWord;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
--- testing.java
+++ testing.java
@@ -2,14 +2,61 @@ public class testing {
 public static void main(String[] args) {
 
 
+    String word = "Wazzzuppp!!!";
+    String word2 = "awzzzppup";
 
-private static double endBalance(double loan, double rate, int n, double payment) {
-for (int i=0; i<n; i++)
-{
-    loan = (loan - payment) * rate;
-}
-return loan;
+    if (isAnagram(word, word2) ==true)
+    System.out.println("yes!!!");
+    else
+    System.out.println("you failed brosky");
 
 }
+
+public static String preProcess(String str) {
+		String word = str.toLowerCase();
+        String newWord = ""; 
+    for (int i = 0; i < word.length(); i++)
+    {
+      char charInWord = word.charAt(i);
+      int locationInAscii = (int) charInWord;
+      if (locationInAscii > 96)
+      {
+        newWord = newWord + charInWord;
+      }
+
+    }
+    System.out.println(newWord);
+	return newWord;
+	} 
+
+
+
+    public static boolean isAnagram(String str1, String str2) {
+		String word1 = preProcess(str1);
+		String word2 = preProcess(str2);
+		int sum1 = 0;
+		int sum2 = 0;
+    for (int i = 0; i < word1.length(); i++)
+    {
+      char charInWord = word1.charAt(i);
+      int locationInAscii = (int) charInWord;
+      sum1 = sum1 + locationInAscii;
+      }
+
+ for (int i = 0; i < word2.length(); i++)
+    {
+      char charInWord = word2.charAt(i);
+      int locationInAscii = (int) charInWord;
+      sum2 = sum2 + locationInAscii;
+    }
+      if (sum1 == sum2)
+       {
+		return true;
+       }
+    else
+      return false;
+	  
+
 }
+
 }
